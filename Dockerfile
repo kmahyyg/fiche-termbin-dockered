76c1913cf768e713f0831d86263d6b1fa67b0c11
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2018-12-15 01:06:10 +0800

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM ubuntu:18.04
+
+VOLUME /data
+ENV CADDYPATH="/data/caddyssl"
+COPY entrypoint.py /usr/bin
+
+RUN apt-get update -y \
+    && apt-get install -y --no-install-recommends build-essential git python3 curl tar supervisord\
+    && git clone https://github.com/solusipse/fiche.git \
+    && chmod +x /usr/bin/entrypoint.py \
+    && curl -LO https://github.com/kmahyyg/deblan_gist_dockered/releases/download/caddy/caddy.tar.bz2 \
+    && tar xvfj caddy.tar.bz2 \
+    && chmod +x caddy \
+    && rm caddy.tar.bz2 \
+    && mkdir -p /data/codes \
+    && mkdir -p /data/caddyssl
+
+WORKDIR /root/fiche
+
+RUN make \
+    && make install
+
+CMD ["/usr/bin/entrypoint.py"]
---
+++ entrypoint.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import sys
+
+confj = '/data/config.json'
+
+stat_confj = os.path.isfile(confj)
+
+flagfile = '/data/configured1'
+
+stat_flag = os.path.isfile(confj)
+
+cmdparams = ['/usr/local/bin/fiche']
+
+spvdconf = "/data/supervisord.conf"
+
+finalcmd = ' '
+
+def procconf(confdict):
+    if isinstance(confdict,dict):
+        try:
+            cmdparams.append('-o')
+            cmdparams.append(confdict['codepath'])
+            cmdparams.append('-d')
+            cmdparams.append(confdict['baseurl'])
+            if confdict['https_enabled'] == True:
+                cmdparams.append('-S')
+            cmdparams.append('-s')
+            cmdparams.append(str(confdict['shorturl']))
+            cmdparams.append('-l')
+            cmdparams.append(confdict['logfile'])
+        except IndexError:
+            sys.exit(2)
+    else:
+        raise TypeError
+
+
+if stat_confj == True:
+    if stat_flag == False:
+        flf = open(flagfile,'w')
+        flf.write('configured')
+        flf.close()
+        confdic = json.loads(open(confj,'r').read())
+        procconf(confdic)
+        finalcmd = finalcmd.join(cmdparams)
+        tempstr = 'command=' + finalcmd
+        fspv = open(spvdconf, 'a')
+        fspv.write(tempstr)
+        fspv.close()
+    else:
+        pass
+    # READ CONFIG AND THEN APPEND TO SUPERVISORD
+    execcmd = ["/usr/bin/supervisord", "-n", "-c", spvdconf]
+    cmdstr = ' '.join(execcmd)
+    os.system(cmdstr)
+else:
+    raise FileNotFoundError
